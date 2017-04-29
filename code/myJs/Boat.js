38603f95f7219b7d70bac75ccd3d9e9dd529d7bc
Huyizhou <hyz@HuyizhoudeMacBook-Pro.local>
2017-04-29 17:12:51 +0800

    boat.js

--- code/myJs/Boat.js
+++ code/myJs/Boat.js
@@ -1,69 +1,215 @@
-function Boat(id) {
+function Boat(scene,camera, id) {
+
     this.id = id;
 
+    var level = 1;
+    var exp = 0;
+    var damage = Math.pow(2, level - 1);
+    var speed = 0.1 * level;
+    var maxExp = 10 * Math.pow(2, level - 1);
+    var giveExp = maxExp / 2;
+    var maxHealth = 3 * Math.pow(2, level - 1);
+    var health = maxHealth;
 
-    this.level = 1;
-    this.damage = Math.pow(2, this.level - 1);
-    this.speed = 10 / this.level;
-    this.exp = 0;
-    this.maxExp = 10 * Math.pow(2, this.level - 1);
-    this.giveExp = this.maxExp / 2;
-    this.maxHealth = 3 * Math.pow(2, this.level - 1);
-    this.health = this.maxHealth;
 
+    var speedTimeDecreaseFlag = false;
 
-    var geometry = new THREE.CubeGeometry(1,1,1);
+    var theta = 0;
+    var curSpeed = 0;
+    //move info
+    var time1, time2;
+
+    var geometry = new THREE.CubeGeometry(1,2,3);
     var material = new THREE.MeshPhongMaterial({color: 0xffffff,shading: THREE.FlatShading});
-    this.body = new THREE.Mesh(geometry, material);
-    this.body.updateMatrix();
+    var body = new THREE.Mesh(geometry, material);
+    body.updateMatrix();
+    body.position.z = 1;
 
+    scene.add(body);
 
     this.bullet = "null"; // todo
 
+
+    this.getBody = function(){
+        return body;
+    };
+
+
     this.fire = function () {
-        alert("fire");
+        // this.bullet.biubiu();
+
     };
 
-    this.move = function () {
-        alert("move");
+
+
+    this.timeExecute = function(){
+        if(Date.now() - time1 >= 500){
+            speedTimeDecreaseFlag = true;
+        }
+        if(speedTimeDecreaseFlag){
+            if (curSpeed > 0){
+                if (curSpeed - 0.002 >= 0)
+                    curSpeed -= 0.002;
+                else{
+                    curSpeed = 0;
+                }
+            }else{
+                if (curSpeed + 0.002 <= 0)
+                    curSpeed += 0.002;
+                else{
+                    curSpeed = 0;
+                }
+            }
+        }
+
+        forward(curSpeed);
+
     };
 
-    this.die = function () {
-        alert("die");
+    this.control = function(key){
+        switch (key){
+            case 'w':
+            case 's':
+            case 'a':
+            case 'd':
+                move(key);
+
+
+        }
     };
 
-    this.changeHealth = function (add_health) {
-        var after_change = this.health + add_health;
-        if (after_change > this.maxHealth) {
-            this.health = this.maxHealth
+
+    var move = function(key){
+        switch(key){
+            case 'w':
+                increaseSpd();
+                time1 = Date.now();
+                speedTimeDecreaseFlag = false;
+                break;
+            case 's':
+                decreaseSpd();
+                time1 = Date.now();
+                speedTimeDecreaseFlag = false;
+                break;
+            case 'a':
+                changeDir(1);
+                break;
+            case 'd':
+                changeDir(-1);
+                break;
+        }
+    };
+
+
+    var increaseSpd = function(){
+        if(curSpeed < speed){
+            if (curSpeed + 0.002 <= speed){
+                curSpeed += 0.002;
+            }else{
+                curSpeed = speed;
+            }
+        }else{
+            curSpeed = speed;
+        }
+    };
+
+    var decreaseSpd = function(){
+        if (curSpeed > -speed){
+            if (curSpeed - 0.002 >= -speed){
+                curSpeed -= 0.002;
+            }else{
+                curSpeed = -speed;
+            }
+        }else{
+            curSpeed = -speed;
+        }
+    };
+
+    var changeDir = function(dir){
+        if (curSpeed != 0){
+            theta += dir;
+            if (theta >= 360){
+                theta -= 360;
+            }
+
+            if (theta <= -360){
+                theta += 360;
+            }
+        }
+
+        var rad = Math.PI / 180;
+        body.rotation.y = rad * theta;
+    };
+
+    var forward = function(dist){
+        var cameraX = camera.position.x;
+        var cameraY = camera.position.y;
+        var cameraZ = camera.position.z;
+        var cameraDirX = camera.rotation.x;
+        var cameraDirY = camera.rotation.y;
+        var cameraDirZ = camera.rotation.z;
+
+        var xDir = body.position.x;
+        var zDir = body.position.z;
+        var rad = Math.PI / 180;
+
+        // var distance = Math.sqrt(xDir * xDir + zDir * zDir);
+        //
+        // distance += dist;
+
+        zDir = zDir + dist * Math.cos(rad * theta);
+        xDir = xDir + dist * Math.sin(rad * theta);
+
+        body.position.z = zDir;
+        body.position.x = xDir;
+
+
+        camera.position.x += dist * Math.sin(rad * theta);
+        camera.position.z += dist * Math.cos(rad * theta);
+        camera.position.y = cameraY;
+        camera.lookAt({
+            x : body.position.x,
+            y : body.position.y,
+            z : body.position.z,
+        });
+
+    };
+
+
+    var changeHealth = function (add_health) {
+        var after_change = health + add_health;
+        if (after_change > maxHealth) {
+            health = maxHealth
         } else if (after_change <= 0) {
             this.die();
         } else {
-            this.health = after_change;
+            health = after_change;
         }
     };
 
-    this.changeExp = function (add_exp) {
+    var changeExp = function (add_exp) {
         if(!this.levelUp(add_exp)){
-            this.exp += add_exp;
+            exp += add_exp;
         }
     };
 
-    this.levelUp = function (added_exp) {
-        var after_exp = this.exp + added_exp;
-        if (after_exp > this.maxExp) {
-            this.level += 1;
-            this.damage = Math.pow(2, this.level - 1);
-            this.speed = 10 / this.level;
-            this.exp = after_exp - this.maxExp;
-            this.maxExp = 10 * Math.pow(2, this.level - 1);
-            this.giveExp = this.maxExp / 2;
-            this.maxHealth = 3 * Math.pow(2, this.level - 1);
-            this.health = this.maxHealth;
+    var levelUp = function (added_exp) {
+        var after_exp = exp + added_exp;
+        if (after_exp > maxExp) {
+            level += 1;
+            damage = Math.pow(2, level - 1);
+            speed = 0.01 * level;
+            exp = after_exp - maxExp;
+            maxExp = 10 * Math.pow(2, level - 1);
+            giveExp = maxExp / 2;
+            maxHealth = 3 * Math.pow(2, level - 1);
+            health = maxHealth;
             return true;
         } else {
             return false;
         }
     };
+
+    setInterval(this.timeExecute,50);
 
 }
--- code/myJs/main.js
+++ code/myJs/main.js
@@ -3,43 +3,65 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 var stats;
 
 var camera, controls, scene, renderer;
-
+var light;
 var boat;
 
+var mouseX = 0, mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+var output = "";
+
+var container;
+
 init();
-render(); // remove when using next line for animation loop (requestAnimationFrame)
-//animate();
+// render(); // remove when using next line for animation loop (requestAnimationFrame)
+animate();
 
 function init() {
     scene = new THREE.Scene();
     scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({ antialias: true } );
     renderer.setClearColor( scene.fog.color );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    var container = document.getElementById( 'container' );
+    container = document.getElementById( 'container' );
+    container.innerHTML =  output ;
+
     container.appendChild( renderer.domElement );
 
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.z = 500;
+    // camera = new THREE.OrthographicCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+
+
+    camera.position.z = 10;
+    camera.position.x = 10;
+    camera.position.y = 10;
 
     controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.addEventListener( 'change', render ); // remove when using animation loop
+    // controls = new THREE.PointerLockControls( camera );
+
+    // controls.addEventListener( 'change', render ); // remove when using animation loop
     // enable animation loop when using damping or autorotation
-    //controls.enableDamping = true;
-    //controls.dampingFactor = 0.25;
-    controls.enableZoom = false;
+    // controls.enableDamping = true;
+    // controls.dampingFactor = 0.25;
+    // controls.enableZoom = false;
 
 
     // world
-    boat = new Boat(1);//todo
-    boat.fire();
+    boat = new Boat(scene,camera, 1);//todo
 
 
-    scene.add(boat.body);
-    camera.position.z = 10;
+
+    //懒得加地面于是这是一个参照物
+    var geometry = new THREE.CubeGeometry(0.5,0.5,0.5);
+    var material = new THREE.MeshPhongMaterial({color: 0xffffff,shading: THREE.FlatShading});
+    var cubeRef = new THREE.Mesh(geometry, material);
+    cubeRef.updateMatrix();
+    scene.add(cubeRef);
+
 
 
     // lights
@@ -55,15 +77,36 @@ function init() {
     light = new THREE.AmbientLight( 0x222222 );
     scene.add( light );
 
-    
-    stats = new Stats();
-    container.appendChild( stats.dom );
+
+    container.appendChild( renderer.domElement );
+
+    // stats = new Stats();
+    // container.appendChild( stats.dom );
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
 
     window.addEventListener( 'resize', onWindowResize, false );
 
+    //control
+    document.addEventListener('keydown',onKeyDown,false);
+
 }
 
+function onKeyDown(event){
+    var value = String.fromCharCode(event.keyCode).toLowerCase();
+
+    boat.control(value);
+    
+}
+
+
+
+
 function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
@@ -72,11 +115,25 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame( animate );
-    controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
-    stats.update();
+    // controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
     render();
+
+
+    // stats.update();
+}
+
+
+function onDocumentMouseMove( event ) {
+    mouseX = ( event.clientX - windowHalfX );
+    mouseY = ( event.clientY - windowHalfY );
+
 }
 
 function render() {
+    // camera.position.x += ( mouseX - camera.position.x ) * 0.1;
+    // camera.position.y += ( - mouseY - camera.position.y ) * 0.1;
+
+
+    // camera.lookAt( scene.position );
     renderer.render( scene, camera );
 }
